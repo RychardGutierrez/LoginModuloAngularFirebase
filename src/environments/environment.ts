3835781d83a2d3737f52bd330db59cdda7c56754
RICHARD GUTIERREZ CONDORI <rychard.gutierrez@gmail.com>
2020-05-17 17:13:05 -0400

    Refactor de modulo login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,6 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyBahocxKp-n3hj5fqM-OHRVP4SqnIjinbs",
-    authDomain: "kuyuy-a261e.firebaseapp.com",
-    databaseURL: "https://kuyuy-a261e.firebaseio.com",
-    projectId: "kuyuy-a261e",
-    storageBucket: "kuyuy-a261e.appspot.com",
-    messagingSenderId: "789473878230",
-    appId: "1:789473878230:web:29995c34f263b1f5162977",
-    measurementId: "G-9FS4G0K9F0"
+    
   }
 };
